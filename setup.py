f3562af7ece6d5aee5d889b7031d7aeffda2df66
Timmy Konick <konick781@gmail.com>
2013-10-09 02:51:59 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from disutils.core import setup
+
+setup(
+    name="quikql",
+    version="0.0.1",
+    author="Tim Konick",
+    author_email="konick781@gmail.com",
+    url="",
+    description="Sqlite3 wrapper",
+    long_description=open('README.md').read(),
+    license=open('LICENSE').read(),
+    packages=["quikql"]
+)
