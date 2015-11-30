13926bc50c52f17a1a0d723ed9f6a84a5a957c8c
Tim Konick <konick781@gmail.com>
2015-11-30 12:47:53 -0500

    Add one more test

--- tests/quikql_test.py
+++ tests/quikql_test.py
@@ -11,64 +11,66 @@ class QuikqlTest(unittest.TestCase):
 
     def setUp(self):
         self.testdb = Quikql(path)
-        self.testdb.create_table('Music', {'artist':'TEXT', 
-                                           'title':'TEXT', 
-                                           'duration':'INTEGER', 
-                                           'track_number':'INTEGER'})
+        self.tablename = 'Music'
+        self.testdb.create_table(self.tablename, {'artist':'TEXT', 
+                                                  'title':'TEXT', 
+                                                  'duration':'INTEGER', 
+                                                  'track_number':'INTEGER'})
 
     def test_create_database(self):
-        test_table = 'Music'
+        test_table = self.tablename
         test_retrieve = self.testdb.get_tables()[0]
         self.assertIn(test_table, test_retrieve)
 
     def test_retrieve_tables(self):
-        tables = 'Music'
+        tables = self.tablename
         test_retrieve = self.testdb.get_tables()[0]
         self.assertIn(tables, test_retrieve)
 
     def test_delete_table(self):
-        self.testdb.delete_table('Music')
+        self.testdb.delete_table(self.tablename)
         tables = self.testdb.get_tables()
         self.assertFalse(tables)
 
     def test_retrieve_schema(self):
         test_schema = {u'artist':u'TEXT', u'title':u'TEXT',
                        u'duration':u'INTEGER', u'track_number':u'INTEGER'}
-        schema = self.testdb.get_schema('Music')
+        schema = self.testdb.get_schema(self.tablename)
         schema_map = {i[1]:i[2] for i in schema}
         self.assertEqual(test_schema, schema_map)
 
     def test_insert_row(self):
-        self.testdb.insert_row('Music', {'artist':'Lifetones', 
+        self.testdb.insert_row(self.tablename, {'artist':'Lifetones', 
                                          'title':'Good Sides'})
-        
-    """
-    def test_get_row(self):
-        row = [(2001,)]
-        self.testdb.insert_row('Music', ['ids'], ['2001'])
-        testrow = self.testdb.get_row('Music', {'ids':'2001'}, size=ALL)
-        self.assertEqual(testrow, row) 
 
+    def test_get_row(self):
+        row = {'artist':'Lifetones', 'title':'Good Sides'}
+        self.testdb.insert_row(self.tablename, row)
+        testrow = self.testdb.get_row(self.tablename, row, size=ALL)
+        retrieved_row = {'artist':testrow[0][2], 'title':testrow[0][1]}
+        self.assertEqual(row, retrieved_row)
+
+    """
     def test_delete_row(self):
-        self.testdb.insert_row('Music', ['ids'], ['2001'])
-        self.testdb.delete_row('Music', 'ids', '2001')
-        table = self.testdb.dump_table('Music')
+        self.testdb.insert_row(self.tablename, ['ids'], ['2001'])
+        self.testdb.delete_row(self.tablename, 'ids', '2001')
+        table = self.testdb.dump_table(self.tablename)
         self.assertEqual([], table)
 
     def test_retrieve_table_content(self):
         table = [(2001,)]
-        self.testdb.delete_table('Music')
-        self.testdb.create_table('Music', {'ids':'INTEGER'})
-        self.testdb.insert_row('Music', ['ids'], ['2001'])
-        testtable = self.testdb.dump_table('Music')
+        self.testdb.delete_table(self.tablename)
+        self.testdb.create_table(self.tablename, {'ids':'INTEGER'})
+        self.testdb.insert_row(self.tablename, ['ids'], ['2001'])
+        testtable = self.testdb.dump_table(self.tablename)
         self.assertEqual(testtable, table)
 
     def test_table_size(self):
         size = 48
-        self.testdb.delete_table('Music')
-        self.testdb.create_table('Music', {'ids':'INTEGER'})
-        self.testdb.insert_row('Music', ['ids'], ['2001'])
-        test_table_size = self.testdb.table_size('Music')
+        self.testdb.delete_table(self.tablename)
+        self.testdb.create_table(self.tablename, {'ids':'INTEGER'})
+        self.testdb.insert_row(self.tablename, ['ids'], ['2001'])
+        test_table_size = self.testdb.table_size(self.tablename)
         self.assertEqual(test_table_size, size)
     """
  
