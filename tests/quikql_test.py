eb6203193e04ace63934b285222bc0082c569231
Tim Konick <konick781@gmail.com>
2015-11-30 13:48:59 -0500

    Add test for table_dump

--- tests/quikql_test.py
+++ tests/quikql_test.py
@@ -12,10 +12,9 @@ class QuikqlTest(unittest.TestCase):
     def setUp(self):
         self.testdb = Quikql(path)
         self.tablename = 'Music'
-        self.testdb.create_table(self.tablename, {'artist':'TEXT', 
-                                                  'title':'TEXT', 
-                                                  'duration':'INTEGER', 
-                                                  'track_number':'INTEGER'})
+        self.schema = {'artist':'TEXT', 'title':'TEXT', 
+                       'duration':'INTEGER', 'track_number':'INTEGER'}
+        self.testdb.create_table(self.tablename, self.schema)
 
     def test_create_database(self):
         test_table = self.tablename
@@ -33,15 +32,13 @@ class QuikqlTest(unittest.TestCase):
         self.assertFalse(tables)
 
     def test_retrieve_schema(self):
-        test_schema = {u'artist':u'TEXT', u'title':u'TEXT',
-                       u'duration':u'INTEGER', u'track_number':u'INTEGER'}
         schema = self.testdb.get_schema(self.tablename)
         schema_map = {i[1]:i[2] for i in schema}
-        self.assertEqual(test_schema, schema_map)
+        self.assertEqual(self.schema, schema_map)
 
     def test_insert_row(self):
         self.testdb.insert_row(self.tablename, {'artist':'Lifetones', 
-                                         'title':'Good Sides'})
+                                                'title':'Good Sides'})
 
     def test_get_row(self):
         row = {'artist':'Lifetones', 'title':'Good Sides'}
@@ -60,23 +57,15 @@ class QuikqlTest(unittest.TestCase):
         self.assertIn(row_retrieve, table_before_del)
         self.assertNotIn(row_retrieve, table_after_del)
 
-    """
     def test_retrieve_table_content(self):
-        table = [(2001,)]
         self.testdb.delete_table(self.tablename)
-        self.testdb.create_table(self.tablename, {'ids':'INTEGER'})
-        self.testdb.insert_row(self.tablename, ['ids'], ['2001'])
+        self.testdb.create_table(self.tablename, self.schema)
+        row = {'artist':'Neal Howard', 'title':'The gathering'}
+        table_retrieve = [(None, u'The gathering', u'Neal Howard', None,)]
+        self.testdb.insert_row(self.tablename, row)
         testtable = self.testdb.dump_table(self.tablename)
-        self.assertEqual(testtable, table)
+        self.assertEqual(testtable, table_retrieve)
 
-    def test_table_size(self):
-        size = 48
-        self.testdb.delete_table(self.tablename)
-        self.testdb.create_table(self.tablename, {'ids':'INTEGER'})
-        self.testdb.insert_row(self.tablename, ['ids'], ['2001'])
-        test_table_size = self.testdb.table_size(self.tablename)
-        self.assertEqual(test_table_size, size)
-    """
  
 if __name__ == '__main__':
     path = os.getcwd() + '/test.db'
