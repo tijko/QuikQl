06b42bf26a42d6901239156dfb0a3f14b0162c55
Tim Konick <konick781@gmail.com>
2015-12-03 23:28:54 -0500

    Edit setUp method to use global variables

--- tests/quikql_test.py
+++ tests/quikql_test.py
@@ -10,60 +10,56 @@ from quikql import *
 class QuikqlTest(unittest.TestCase):
 
     def setUp(self):
-        self.testdb = Quikql(path)
-        self.tablename = 'Music'
-        self.schema = {'artist':'TEXT', 'title':'TEXT', 
-                       'duration':'INTEGER', 'track_number':'INTEGER'}
-        self.testdb.create_table(self.tablename, self.schema)
+        testdb.create_table(tablename, schema)
 
     def test_create_database(self):
-        test_table = self.tablename
-        test_retrieve = self.testdb.get_tables()[0]
+        test_table = tablename
+        test_retrieve = testdb.get_tables()[0]
         self.assertIn(test_table, test_retrieve)
 
     def test_retrieve_tables(self):
-        tables = self.tablename
-        test_retrieve = self.testdb.get_tables()[0]
+        tables = tablename
+        test_retrieve = testdb.get_tables()[0]
         self.assertIn(tables, test_retrieve)
 
     def test_delete_table(self):
-        self.testdb.delete_table(self.tablename)
-        tables = self.testdb.get_tables()
+        testdb.delete_table(tablename)
+        tables = testdb.get_tables()
         self.assertFalse(tables)
 
     def test_retrieve_schema(self):
-        schema = self.testdb.get_schema(self.tablename)
-        schema_map = {i[1]:i[2] for i in schema}
-        self.assertEqual(self.schema, schema_map)
+        test_schema = testdb.get_schema(tablename)
+        schema_map = {i[1]:i[2] for i in test_schema}
+        self.assertEqual(schema, schema_map)
 
     def test_insert_row(self):
-        self.testdb.insert_row(self.tablename, {'artist':'Lifetones', 
-                                                'title':'Good Sides'})
+        testdb.insert_row(tablename, {'artist':'Lifetones', 
+                                      'title':'Good Sides'})
 
     def test_get_row(self):
         row = {'artist':'Lifetones', 'title':'Good Sides'}
-        self.testdb.insert_row(self.tablename, row)
-        testrow = self.testdb.get_row(self.tablename, row, size=ALL)
+        testdb.insert_row(tablename, row)
+        testrow = testdb.get_row(tablename, row, size=ALL)
         retrieved_row = {'artist':testrow[0][2], 'title':testrow[0][1]}
         self.assertEqual(row, retrieved_row)
 
     def test_delete_row(self):
         row = {'artist':'Neal Howard', 'title':'The gathering'}
         row_retrieve = (None, u'The gathering', u'Neal Howard', None,)
-        self.testdb.insert_row(self.tablename, row)
-        table_before_del = self.testdb.dump_table(self.tablename)
-        self.testdb.delete_row(self.tablename, row)
-        table_after_del = self.testdb.dump_table(self.tablename)
+        testdb.insert_row(tablename, row)
+        table_before_del = testdb.dump_table(tablename)
+        testdb.delete_row(tablename, row)
+        table_after_del = testdb.dump_table(tablename)
         self.assertIn(row_retrieve, table_before_del)
         self.assertNotIn(row_retrieve, table_after_del)
 
     def test_retrieve_table_content(self):
-        self.testdb.delete_table(self.tablename)
-        self.testdb.create_table(self.tablename, self.schema)
+        testdb.delete_table(tablename)
+        testdb.create_table(tablename, schema)
         row = {'artist':'Neal Howard', 'title':'The gathering'}
         table_retrieve = [(None, u'The gathering', u'Neal Howard', None,)]
-        self.testdb.insert_row(self.tablename, row)
-        testtable = self.testdb.dump_table(self.tablename)
+        testdb.insert_row(tablename, row)
+        testtable = testdb.dump_table(tablename)
         self.assertEqual(testtable, table_retrieve)
 
  
@@ -71,4 +67,8 @@ if __name__ == '__main__':
     path = os.getcwd() + '/test.db'
     if os.path.isfile(path):
         os.unlink(path)
+    testdb = Quikql(path)
+    tablename = 'Music'
+    schema = {'artist':'TEXT', 'title':'TEXT', 
+              'duration':'INTEGER', 'track_number':'INTEGER'}
     unittest.main(verbosity=3) 
